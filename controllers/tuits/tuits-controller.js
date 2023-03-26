7d28077bc2b52c4e291a59a96b2ac2ba7d783d2f
katherine-payne <kamapa001@gmail.com>
2023-03-25 20:51:29 -0400

    A8 Node

---
+++ controllers/tuits/tuits-controller.js
@@ -0,0 +1,52 @@
+import posts from "./tuits.js";
+
+let tuits = posts;
+
+const currentUser = {
+    "userName": "NASA",
+    "handle": "@nasa",
+    "image": "nasa.png",
+};
+
+const templateTuit = {
+    ...currentUser,
+    "topic": "Space",
+    "time": "2h",
+    "replies": 0,
+    "retuits": 0,
+}
+
+const createTuit = (req, res) => {
+    let newTuit = req.body;
+    newTuit = {...templateTuit, ...newTuit}
+    newTuit._id = (new Date()).getTime() + '';
+    newTuit.likes = 0;
+    newTuit.liked = false;
+    newTuit.dislikes = 0;
+    newTuit.disliked = false;
+    tuits.push(newTuit);
+    res.json(newTuit);
+};
+
+const findTuits = (req, res) => res.json(tuits);
+const updateTuit = (req, res) => {
+    const tuitIdToUpdate = req.params.tid;
+    const updates = req.body;
+    const tuitIndex = tuits.findIndex((t) => t._id === tuitIdToUpdate);
+    tuits[tuitIndex] = {...tuits[tuitIndex], ...updates};
+    res.sendStatus(200);
+};
+
+const deleteTuit = (req, res) => {
+    const tuitdIdToDelete = req.params.tid;
+    tuits = tuits.filter((t) => t._id !== tuitdIdToDelete);
+    res.sendStatus(200);
+};
+
+
+export default (app) => {
+    app.post('/api/tuits', createTuit);
+    app.get('/api/tuits', findTuits);
+    app.put('/api/tuits/:tid', updateTuit);
+    app.delete('/api/tuits/:tid', deleteTuit);
+}
